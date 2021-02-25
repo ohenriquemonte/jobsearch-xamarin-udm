9207ec399caaf0e420d5b7efbbe81ea2a11f1cc3
ohenriquemonte <montenegrohenrique@gmail.com>
2021-02-25 12:30:38 -0300

    281. 04 - API - Configurando o Banco - EFCore e Context

---
+++ JobSearch.API/Database/JobSearchContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace JobSearch.API.Database
+{
+	public class JobSearchContext : DbContext
+	{
+		public JobSearchContext()
+		{
+		}
+	}
+}
--- JobSearch.API/Startup.cs
+++ JobSearch.API/Startup.cs
@@ -10,42 +10,49 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using JobSearch.API.Database;
 
 namespace JobSearch.API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<JobSearchContext>(options =>
+			{
+				Configuration.GetConnectionString("DefaultConnection");
+				options.UseSqlite();
+			});
+			services.AddControllers();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
