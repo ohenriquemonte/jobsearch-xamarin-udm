872638a95a275171cf67eedcfb7b1adb4465f3ac
Henrique Bueno Montenegro <montenegrohenrique@outlook.com>
2021-03-01 18:59:34 -0300

    291.14 - API - Traduzir Mensagens de Validação

--- JobSearch.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JobSearch.Domain
-{
-    public class Class1
-    {
-    }
-}
--- JobSearch.Domain/JobSearch.Domain/Models/User.cs
+++ JobSearch.Domain/JobSearch.Domain/Models/User.cs
@@ -8,16 +8,19 @@ namespace JobSearch.Domain.Models
 		[Key]
 		public int Id { get; set; }
 
-		[Required]
-		[MinLength(10)]
+		[Display(Name="Name", ResourceType = typeof(JobSearch.Domain.Utility.Language.Fields))]
+		[Required(ErrorMessageResourceType = typeof(JobSearch.Domain.Utility.Language.Messages), ErrorMessageResourceName = "MSG_E001")]
+		[MinLength(10, ErrorMessageResourceType = typeof(JobSearch.Domain.Utility.Language.Messages), ErrorMessageResourceName = "MSG_E003")]
 		public string Name { get; set; }
 
-		[Required]
-		[EmailAddress]
+		[Display(Name = "Email", ResourceType = typeof(JobSearch.Domain.Utility.Language.Fields))]
+		[Required(ErrorMessageResourceType = typeof(JobSearch.Domain.Utility.Language.Messages), ErrorMessageResourceName = "MSG_E001")]
+		[EmailAddress(ErrorMessageResourceType = typeof(JobSearch.Domain.Utility.Language.Messages), ErrorMessageResourceName = "MSG_E002")]
 		public string Email { get; set; }
 
-		[Required]
-		[MinLength(6)]
+		[Display(Name = "Password", ResourceType = typeof(JobSearch.Domain.Utility.Language.Fields))]
+		[Required(ErrorMessageResourceType = typeof(JobSearch.Domain.Utility.Language.Messages), ErrorMessageResourceName = "MSG_E001")]
+		[MinLength(6, ErrorMessageResourceType = typeof(JobSearch.Domain.Utility.Language.Messages), ErrorMessageResourceName = "MSG_E003")]
 		public string Password { get; set; }
 
 		//public User()
