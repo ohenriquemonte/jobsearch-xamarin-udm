986e19a54f9722b933a09c6811f3bbbc482cd40b
ohenriquemonte <montenegrohenrique@gmail.com>
2021-03-02 10:19:18 -0300

    293. 16 - APP - Android - SplashScreen e Icone

--- JobSearch.APP.Android/MainActivity.cs
+++ JobSearch.APP.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace JobSearch.APP.Droid
 {
-    [Activity(Label = "JobSearch.APP", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
